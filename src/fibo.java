d08f96d4466ae6534c7973f9610fe033d962afcd
lenuyx_R_Löhr <lenuyx@live.com>
2015-05-22 10:30:21 +0200

    Update: fibo, schreibt in array

--- src/fibo.java
+++ src/fibo.java
@@ -1,21 +1,27 @@
 public class fibo {
 
+	static int a[] = new int[21];
 	public static void main(String[] args) {
 
 		int i = 20;
-		int a[] = new int[20];
-		a[0] = fibos(i);
-		System.out.println(a[0]);
+		a[0] = 1;
+		a[1] = 1;
+		int answer = fibos(i);
+		System.out.println(answer);
+		for (int j = 0; j < a.length; j++) {
+			System.out.println(a[j]);
+		}
+		
 
 	}
 
 	public static int fibos(int i) {
-		if (i <= 0) {
-			return 0;
+		if (i == 0) {
+			return a[0];
 		} else if (i == 1) {
-			return 1;
+			return a[1];
 		} else {
-			return fibos(i - 2) + fibos(i - 1);
+			return a[i] = fibos(i - 2) + fibos(i - 1);
 		}
 	}
 }
